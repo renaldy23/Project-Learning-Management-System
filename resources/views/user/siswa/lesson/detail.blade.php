c2da15bd955c207b0e3bc5cb3f2199862ae5d921
renaldy23 <renaldyramadhan567@gmail.com>
2021-05-29 16:39:51 +0700

    Fix View

--- resources/views/user/siswa/lesson/detail.blade.php
+++ resources/views/user/siswa/lesson/detail.blade.php
@@ -10,53 +10,78 @@
     <div id="underline"></div>
     <div class="row mt-2 flex-column-reverse flex-md-row">
         <div class="col-lg-8 mt-3 mw-100">
-            <div class="container mw-100 bg-white p-3 shadow rounded-lg">
-                <ul class="nav nav-pills mb-3 nav-fill" id="pills-tab" role="tablist">
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" 
-                        aria-selected="true">Lesson</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link" 
-                        id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" 
-                        aria-selected="false">Task</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link" 
-                        id="pills-quiz-tab" data-toggle="pill" href="#pills-quiz" role="tab" aria-controls="pills-quiz" 
-                        aria-selected="false">Quiz</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Presence</a>
-                    </li>
-                </ul>
-                <div id="underline"></div>
-                <div class="tab-content" id="pills-tabContent">
-                    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <ul class="list-group mt-2">
-                                    @foreach ($course->lesson as $item)
-                                        <li class="list-group-item" style="font-weight: normal">
-                                            <a href="{{ route("siswa.lesson.detail",["id" => $item->id]) }}">{{ $item->title }}</a>
-                                        </li>
-                                    @endforeach
-                                </ul>
+            <div class="card">
+                <div class="card-header">
+                    {{ $course->guru->name }}
+                </div>
+                <div class="card-body">
+                    <div class="container mw-100">
+                        <ul class="nav nav-pills mb-3 nav-fill" id="pills-tab" role="tablist">
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" 
+                                aria-selected="true">Lesson</a>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link" 
+                                id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" 
+                                aria-selected="false">Task</a>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link" 
+                                id="pills-quiz-tab" data-toggle="pill" href="#pills-quiz" role="tab" aria-controls="pills-quiz" 
+                                aria-selected="false">Quiz</a>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Presence</a>
+                            </li>
+                        </ul>
+                        <div id="underline"></div>
+                        <div class="tab-content" id="pills-tabContent">
+                            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <ul class="list-group mt-2">
+                                            @foreach ($course->lesson as $item)
+                                                <li class="list-group-item" style="font-weight: normal">
+                                                    <a href="{{ route("siswa.lesson.detail",["id" => $item->id]) }}">{{ $item->title }}</a>
+                                                </li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="row mt-3">
+                            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <div class="row mt-3">
+                                            <div class="col-sm-12">
+                                                @foreach ($course->lesson as $item)
+                                                    @if ($item->task->count()!=0)
+                                                        <p style="font-weight: normal" class="mb-1">{{ $item->title }}</p>
+                                                        <ul class="list-group mb-4">
+                                                            @foreach ($item->task as $data)
+                                                                <li class="list-group-item" style="font-weight: normal">
+                                                                    <a href="{{ route("siswa.task.detail",["id" => $data->id]) }}">{{ $data->title }}</a>
+                                                                </li>
+                                                            @endforeach
+                                                        </ul>
+                                                    @endif
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="pills-quiz" role="tabpanel" aria-labelledby="pills-quiz-tab">
+                                <div class="row">
                                     <div class="col-sm-12">
                                         @foreach ($course->lesson as $item)
-                                            @if ($item->task->count()!=0)
-                                                <p style="font-weight: normal" class="mb-1">{{ $item->title }}</p>
+                                            @if ($item->quiz->count()!=0)
+                                            <p style="font-weight: normal" class="mb-1">{{ $item->title }}</p>
                                                 <ul class="list-group mb-4">
-                                                    @foreach ($item->task as $data)
+                                                    @foreach ($item->quiz as $data)
                                                         <li class="list-group-item" style="font-weight: normal">
-                                                            <a href="{{ route("siswa.task.detail",["id" => $data->id]) }}">{{ $data->title }}</a>
+                                                            <a href="{{ route("lesson.quiz.detail",["id" => $data->id]) }}?course_id={{ $course->id }}">{{ $data->title }}</a>
                                                         </li>
                                                     @endforeach
                                                 </ul>
@@ -65,58 +90,40 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="pills-quiz" role="tabpanel" aria-labelledby="pills-quiz-tab">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                @foreach ($course->lesson as $item)
-                                    @if ($item->quiz->count()!=0)
-                                    <p style="font-weight: normal" class="mb-1">{{ $item->title }}</p>
-                                        <ul class="list-group mb-4">
-                                            @foreach ($item->quiz as $data)
-                                                <li class="list-group-item" style="font-weight: normal">
-                                                    <a href="{{ route("lesson.quiz.detail",["id" => $data->id]) }}?course_id={{ $course->id }}">{{ $data->title }}</a>
+                            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <ul class="list-group">
+                                            @foreach ($presence as $p)
+                                                <li class="list-group-item">
+                                                    <p style="font-weight: normal" class="mb-1">
+                                                        {{ $p->title }}
+                                                    </p>
+                                                    @if ($p->siswapresence->count()==0)
+                                                        <form action="{{ route("presence.attempt",["id" => $p->id]) }}" method="post">
+                                                            @csrf
+                                                            <button type="submit" class="btn-sm btn btn-success">Precense</button>
+                                                        </form>
+                                                    @else
+                                                        @foreach ($p->siswapresence as $item)
+                                                            @if ($item->status=="done")
+                                                                <p class="text-muted mb-1" style="font-size: 15px;">Presence Confirmed</p>
+                                                                <p class="mb-1 text-muted" style="font-size: 14px">Presence at {{ $item->created_at }}</p>
+                                                            @elseif($item->status=="late")
+                                                                @php
+                                                                    $startTime  = \Carbon\Carbon::parse($item->created_at);
+                                                                    $endTime  = \Carbon\Carbon::parse($p->due_date);
+                                                                    $diff_in_days = $startTime->diff($endTime)->format('%D Days %H Hours %I Minutes %S Seconds');
+                                                                @endphp
+                                                                <p class="text-danger mb-1">Overdue By <br> {{ $diff_in_days }}</p>
+                                                            @endif
+                                                        @endforeach
+                                                    @endif
                                                 </li>
                                             @endforeach
                                         </ul>
-                                    @endif
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <ul class="list-group">
-                                    @foreach ($presence as $p)
-                                        <li class="list-group-item">
-                                            <p style="font-weight: normal" class="mb-1">
-                                                {{ $p->title }}
-                                            </p>
-                                            @if ($p->siswapresence->count()==0)
-                                                <form action="{{ route("presence.attempt",["id" => $p->id]) }}" method="post">
-                                                    @csrf
-                                                    <button type="submit" class="btn-sm btn btn-success">Precense</button>
-                                                </form>
-                                            @else
-                                                @foreach ($p->siswapresence as $item)
-                                                    @if ($item->status=="done")
-                                                        <p class="text-muted mb-1" style="font-size: 15px;">Presence Confirmed</p>
-                                                        <p class="mb-1 text-muted" style="font-size: 14px">Presence at {{ $item->created_at }}</p>
-                                                    @elseif($item->status=="late")
-                                                        @php
-                                                            $startTime  = \Carbon\Carbon::parse($item->created_at);
-                                                            $endTime  = \Carbon\Carbon::parse($p->due_date);
-                                                            $diff_in_days = $startTime->diff($endTime)->format('%D Days %H Hours %I Minutes %S Seconds');
-                                                        @endphp
-                                                        <p class="text-danger mb-1">Overdue By <br> {{ $diff_in_days }}</p>
-                                                    @endif
-                                                @endforeach
-                                            @endif
-                                        </li>
-                                    @endforeach
-                                </ul>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
